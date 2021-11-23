4a480a5746f1386f95b1184f83ba52a5784e19a7
OysteinThorheim <81231170+OysteinThorheim@users.noreply.github.com>
2021-11-23 12:55:21 +0100

    Initial Commit

---
+++ TemaOppgaveOdaSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemaOppgaveOdaSwiftUI
+//
+//  Created by Øystein Thorheim on 23/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TemaOppgaveOdaSwiftUI/TemaOppgaveOdaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemaOppgaveOdaSwiftUIApp.swift
+//  TemaOppgaveOdaSwiftUI
+//
+//  Created by Øystein Thorheim on 23/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TemaOppgaveOdaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
